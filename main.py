5fc328b6d460813274195e6cb6baba9b9afa2ef6
Ramona Fuchs <65505215+raexune@users.noreply.github.com>
2022-04-21 09:42:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import discord
+import requests
+import json
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]["q"] + " -" + json_data[0]["a"]
+  return(quote)
+
+my_secret = os.environ['token']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("Logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith("!inspire"):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+client.run(my_secret)
+  
+
+        
+
+
+  
+  
